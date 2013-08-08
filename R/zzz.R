3c7a57cbbafb2f41a8df822808075075a04e9f9b
Bin Wang <bwang@southalabama.edu>
2013-08-08 00:00:00 +0000

    version 1.1.8-8

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,4 @@
-.onLoad <- function(lib, pkg){
-  packageStartupMessage("gb 1.0 loaded\nCopyright B. Wang 2011")
-  assign('.gbConnect',NULL,pos=.GlobalEnv) 
-}
-
 .onUnload <- function(libpath)
     library.dynam.unload("gb",  libpath)
 
 .gbConnect <- NULL
-
-##  This line should be kept in NAMESPACE: export(ofckde, ofcem, ofcmm, ofcscb, print.scb, plot.scb,lines.scb, edf, print.edf, plot.edf, mixnorm)
